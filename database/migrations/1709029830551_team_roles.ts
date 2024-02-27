94c9afc0e5d8670006fb43072baf01b9502124cc
RemiSaurel <r.saurel5@gmail.com>
2024-02-27 11:41:05 +0100

    db: team_role administration migration

--- database/migrations/1709029830551_team_roles.ts
+++ database/migrations/1709029830551_team_roles.ts
@@ -1,21 +1,42 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+import TeamRoles from 'App/Enums/TeamRoles'
 
 export default class extends BaseSchema {
   protected tableName = 'team_roles'
+  protected team_user = 'team_user'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.string('name').notNullable()
+    })
 
-      /**
-       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
-       */
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+    this.defer(async () => {
+      const roles = [
+        { id: TeamRoles.USER, name: 'user' },
+        { id: TeamRoles.ADMIN, name: 'admin' },
+      ]
+      await this.db.insertQuery().table(this.tableName).multiInsert(roles)
+    })
+
+    // Add team_role_id to team_user
+    this.schema.alterTable(this.team_user, (table) => {
+      table
+        .integer('team_role_id')
+        .unsigned()
+        .references('id')
+        .inTable(this.tableName)
+        .onDelete('CASCADE')
+        .defaultTo(TeamRoles.USER)
+    })
+
+    // Update team_user table by setting all users to admin
+    this.defer(async () => {
+      await this.db.from(this.team_user).update('team_role_id', TeamRoles.ADMIN)
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
