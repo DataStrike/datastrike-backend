d5623d090a2c1a886250e1440f4d02bbeec6979f
RemiSaurel <r.saurel5@gmail.com>
2024-02-27 12:50:46 +0100

    chore: add userId

--- app/Services/TeamsService.ts
+++ app/Services/TeamsService.ts
@@ -3,7 +3,7 @@ import User from 'App/Models/User'
 class TeamsService {
   public async getPlayers(teamId: number): Promise<User[]> {
     return User.query()
-      .select(['name', 'email', 'avatar_url'])
+      .select(['users.id', 'name', 'email', 'avatar_url'])
       .innerJoin('team_user', 'users.id', 'team_user.user_id')
       .where('team_user.team_id', teamId)
   }
